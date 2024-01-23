aa478755adf5c7f90eddad6975b93679ac634e6d
Talish Leonid <lenkat44437@gmail.com>
2024-01-23 17:05:58 +0300

    3 task: calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,22 @@
 let calculator = {
-  // ваш код
+  massive: {}, 
+  read (a, b) {
+    return this.massive = {a, b}; 
+  },
+  sum () {
+    let sum = 0;
+    for (let key in this.massive) {
+      sum += this.massive[key];
+    }
+    return sum;
+  },
+  mul () {
+    let mul = 1;
+    for (let key in this.massive) {
+      mul *= this.massive[key];
+    }
+    return mul;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
