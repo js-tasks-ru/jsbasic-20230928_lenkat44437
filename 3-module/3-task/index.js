ab31621d6b331499b69465ec790f8bad79515ab0
Talish Leonid <lenkat44437@gmail.com>
2024-02-12 17:30:05 +0300

    4 task: massive camalize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
-function camelize(str) {
-  // ваш код...
+function camelize(text) { 
+  let startmassive = text.split(`-`);
+  let result = [];
+  for (let i = 0; i < startmassive.length; i++) {
+    if (startmassive[i]) {
+      if (startmassive.indexOf(startmassive[i])) {
+        let a = startmassive[i][0].toUpperCase() + startmassive[i].slice(1).toLowerCase();
+        result.push(a);
+      } else {
+        let a = startmassive[0].toLowerCase(); 
+        result.push(a);
+      }
+    }
+  }
+  return result.join('');
 }
