835f3cce236141261605b4d4d310bd2253bc647d
Talish Leonid <lenkat44437@gmail.com>
2024-02-19 18:06:19 +0300

    5 module: 2 task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('button');
+  let hidestText = document.getElementById('text');
+  function hide () {
+    hidestText.toggleAttribute('hidden');
+  }
+  return button.addEventListener('click', hide);
 }
+  
