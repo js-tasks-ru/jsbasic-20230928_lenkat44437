57daaff5c549c3bf8c5a89229b2ea6777fbbd1e3
Talish Leonid <lenkat44437@gmail.com>
2024-02-28 11:38:48 +0300

    6 module: 3 task v2

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,7 +3,7 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
-    this.elem = this.changeCarusel();
+    this.elem = this.render();
   }
 
   render () {
